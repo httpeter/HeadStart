82163d3e50b5fcef22bc3f472172aa6f6ce59cfd
unknown <PeterH@Porter.furore.com>
2016-02-08 14:01:25 +0100

    moved to maven

---
+++ src/main/java/com/furore/util/AESEncryptor.java
@@ -0,0 +1,72 @@
+// This code covered by the Apache2 License: http://www.apache.org/licenses/LICENSE-2.0
+// You are free to use it for your own good as long as it doesn't hurt anybody.
+// For questions or suggestions please contact me at httpeter@gmail.com
+package com.furore.util;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+public class AESEncryptor implements Serializable
+{
+
+    private final Cipher encryptor = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    private final Cipher decryptor = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+
+
+    public AESEncryptor() throws Exception
+    {
+        ExternalContext externalContext = FacesContext.getCurrentInstance()
+                .getExternalContext();
+
+        String sessionKey = externalContext.getInitParameter("sixteenBitEncryptionKey");
+        String iv = externalContext.getInitParameter("sixteenBitEncryptionSalt");
+
+        byte[] keyBytes;
+        byte[] vectorBytes;
+
+        //new BASE64Decoder().decodeBuffer(sessionKey);
+        keyBytes = sessionKey.getBytes();
+        //new BASE64Decoder().decodeBuffer(iv);
+        vectorBytes = iv.getBytes();
+
+        encryptor.init(Cipher.ENCRYPT_MODE,
+                new SecretKeySpec(keyBytes, "AES"),
+                new IvParameterSpec(vectorBytes));
+
+        decryptor.init(Cipher.DECRYPT_MODE,
+                new SecretKeySpec(keyBytes, "AES"),
+                encryptor.getParameters());
+    }
+
+
+
+    public String encrypt(String plainText) throws UnsupportedEncodingException,
+            IllegalBlockSizeException,
+            BadPaddingException
+    {
+        // get bytes from string, encrypt, encode
+        byte[] utf8bytes = plainText.getBytes("utf-8");
+        byte[] ciphertext = encryptor.doFinal(utf8bytes);
+        return Base64.getEncoder().encodeToString(ciphertext);
+    }
+
+
+
+    public String decrypt(String cipherText) throws IOException,
+            IllegalBlockSizeException,
+            BadPaddingException
+    {
+        // decode, decrypt, use bytes to create string
+        byte[] encryptedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] plaintext = decryptor.doFinal(encryptedBytes);
+        return new String(plaintext, "UTF-8");
+    }
+}
---
+++ src/main/java/com/furore/util/FMessage.java
@@ -0,0 +1,57 @@
+// This code covered by the Apache2 License: http://www.apache.org/licenses/LICENSE-2.0
+// You are free to use it for your own good as long as it doesn't hurt anybody.
+// For questions or suggestions please contact me at httpeter@gmail.com
+package com.furore.util;
+
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class FMessage implements Serializable
+{
+
+    private static final Logger logger = Logger.getLogger(FMessage.class.getName());
+
+
+
+    public final void warn(String msg)
+    {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_WARN,
+                        msg, null));
+        logger.log(Level.WARNING, msg);
+    }
+
+
+
+    public final void error(String msg)
+    {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                        msg, null));
+        logger.log(Level.WARNING, msg);
+    }
+
+
+
+    public final void fatal(String msg)
+    {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_FATAL,
+                        msg, null));
+        logger.log(Level.WARNING, msg);
+    }
+
+
+
+    public final void info(String msg)
+    {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO,
+                        msg, null));
+        logger.log(Level.INFO, msg);
+    }
+
+}
