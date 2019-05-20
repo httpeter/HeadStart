ed6cef98cdef0266a2699f206eb5c2e1d59245f2
Peter Hendriks <phendriks@servoy.com>
2019-05-20 15:41:15 +0200

    Restored weird modification of POM file

--- src/main/java/case1/nl/entities/Person.java
+++ src/main/java/case1/nl/entities/Person.java
@@ -7,7 +7,6 @@ package case1.nl.entities;
 
 import java.io.Serializable;
 import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,46 +15,38 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author peterhendriks
  */
 @Entity
-@Table(name = "Person")
-@XmlRootElement
+@Table(catalog = "example", schema = "")
 @NamedQueries({
-    @NamedQuery(name = "Person.findAll", query = "SELECT p FROM Person p")
-    , @NamedQuery(name = "Person.findById", query = "SELECT p FROM Person p WHERE p.id = :id")
-    , @NamedQuery(name = "Person.findByFirstName", query = "SELECT p FROM Person p WHERE p.firstName = :firstName")
-    , @NamedQuery(name = "Person.findByLastName", query = "SELECT p FROM Person p WHERE p.lastName = :lastName")
-    , @NamedQuery(name = "Person.findByAge", query = "SELECT p FROM Person p WHERE p.age = :age")
-    , @NamedQuery(name = "Person.findByGender", query = "SELECT p FROM Person p WHERE p.gender = :gender")})
+    @NamedQuery(name = "Person.findAll", query = "SELECT p FROM Person p"),
+    @NamedQuery(name = "Person.findById", query = "SELECT p FROM Person p WHERE p.id = :id"),
+    @NamedQuery(name = "Person.findByAge", query = "SELECT p FROM Person p WHERE p.age = :age"),
+    @NamedQuery(name = "Person.findByFirstName", query = "SELECT p FROM Person p WHERE p.firstName = :firstName"),
+    @NamedQuery(name = "Person.findByGender", query = "SELECT p FROM Person p WHERE p.gender = :gender"),
+    @NamedQuery(name = "Person.findByLastName", query = "SELECT p FROM Person p WHERE p.lastName = :lastName"),
+    @NamedQuery(name = "Person.findByNotes", query = "SELECT p FROM Person p WHERE p.notes = :notes")})
 public class Person implements Serializable {
-
-    @Size(max = 255)
-    @Column(name = "age")
-    private String age;
-    @Size(max = 255)
-    @Column(name = "firstName")
-    private String firstName;
-    @Size(max = 255)
-    @Column(name = "gender")
-    private String gender;
-    @Size(max = 255)
-    @Column(name = "lastName")
-    private String lastName;
-    @Size(max = 500)
-    @Column(name = "notes")
-    private String notes;
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
-    @Column(name = "id")
     private Integer id;
+    @Size(max = 255)
+    private String age;
+    @Size(max = 255)
+    private String firstName;
+    @Size(max = 255)
+    private String gender;
+    @Size(max = 255)
+    private String lastName;
+    @Size(max = 500)
+    private String notes;
 
     public Person() {
     }
@@ -70,32 +61,6 @@ public class Person implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Person)) {
-            return false;
-        }
-        Person other = (Person) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "case1.nl.entities.Person[ id=" + id + " ]";
     }
 
     public String getAge() {
@@ -136,6 +101,31 @@ public class Person implements Serializable {
 
     public void setNotes(String notes) {
         this.notes = notes;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "case1.nl.entities.Person[ id=" + id + " ]";
     }
     
 }
