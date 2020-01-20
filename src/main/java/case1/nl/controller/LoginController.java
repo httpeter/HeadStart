0c4bfaefe108a688cc99ccbcc339662fd9c28833
Peter hendriks <phendriks@servoy.com>
2020-01-20 15:10:59 +0100

    shizzl

--- src/main/java/case1/nl/controller/LoginController.java
+++ src/main/java/case1/nl/controller/LoginController.java
@@ -72,7 +72,7 @@ public class LoginController implements Serializable {
                     + mail
                     + " logged in.");
             session.setCurrentUser(user);
-            return "index.html";
+            return "s3.html";
 
         } else {
             FMessage.warn("Problem logging in user '"
