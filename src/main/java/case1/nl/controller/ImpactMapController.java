2a2eb4dafc81880d6b8dc4e4b527556b91a835b9
Peter hendriks <phendriks@servoy.com>
2020-03-25 12:28:09 +0100

    Updated PF and playing with treenodes...

--- src/main/java/case1/nl/controller/ImpactMapController.java
+++ src/main/java/case1/nl/controller/ImpactMapController.java
@@ -18,35 +18,35 @@ import org.primefaces.model.TreeNode;
 @ViewScoped
 @ManagedBean
 public class ImpactMapController implements Serializable {
-
+    
     @ManagedProperty(value = "#{sessionController}")
     private SessionController session;
-
+    
     private List<TreeNode> impactMaps;
-
+    
     private TreeNode selectedIM;
 
     //<editor-fold defaultstate="collapsed" desc="Getters & Setters">
     public List<TreeNode> getImpactMaps() {
         return impactMaps;
     }
-
+    
     public void setImpactMaps(List<TreeNode> impactMaps) {
         this.impactMaps = impactMaps;
     }
-
+    
     public TreeNode getSelectedIM() {
         return selectedIM;
     }
-
+    
     public void setSelectedIM(TreeNode selectedIM) {
         this.selectedIM = selectedIM;
     }
-
+    
     public SessionController getSession() {
         return session;
     }
-
+    
     public void setSession(SessionController session) {
         this.session = session;
     }
@@ -54,31 +54,41 @@ public class ImpactMapController implements Serializable {
     //</editor-fold>
     public ImpactMapController() {
     }
-
+    
     @PostConstruct
     public void init() {
-
-        impactMaps = new ArrayList();
-
-    }
-
-    public void newIM() {
-
-        TreeNode impactMap = new DefaultTreeNode("Goal:", null);
-
-        TreeNode actor = new DefaultTreeNode("Actor:", impactMap);
-
-        TreeNode imapct = new DefaultTreeNode("Impact:", actor);
-
-        TreeNode deliverable = new DefaultTreeNode("Deliverable(epic):", imapct);
-
-        TreeNode userStory = new DefaultTreeNode("UserStory:", deliverable);
-
-        impactMap.getChildren().forEach(child -> {
-            System.out.println("test");
-        });
         
-    impactMaps.add (impactMap);
-}
-
+        impactMaps = new ArrayList();
+        
+    }
+    
+    public void newIM() {
+        
+        TreeNode impactMap = new DefaultTreeNode("Goal:", null);
+        
+        TreeNode actor = new DefaultTreeNode("Actor:", impactMap);
+        
+        TreeNode imapct = new DefaultTreeNode("Impact:", actor);
+        
+        TreeNode deliverable = new DefaultTreeNode("Deliverable(epic):", imapct);
+        
+        TreeNode userStory = new DefaultTreeNode("UserStory:", deliverable);
+        
+        impactMaps.add(impactMap);
+        
+    }
+    
+    public void showChildren() {
+        if (!impactMaps.isEmpty()) {
+            impactMaps.forEach(im -> {
+                FMessage.info("Goal: '"im.getData().toString()+"' children: " +im.getChildren().toString());
+                
+            });
+            
+        } else {
+            FMessage.error("No maps found");
+        }
+        
+    }
+    
 }
